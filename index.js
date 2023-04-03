335670891b214538f3c1f538493bd7dfe09af16f
nicolas ngwira <ngwiranicolas@gmail.com>
2023-04-03 17:54:54 +0200

    added data fetching and css

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 let dataHolder=document.getElementById("content");
+let form=document.getElementById("search")
+
+const formSub=async (e)=>{
+    e.preventDefault();
+    dataHolder.innerHTML=``;
+    let formData=new FormData(form);
+    let city=formData.get("city");
+
+    let url=`http://api.weatherapi.com/v1/current.json?key=91de4c405b304512a41210049230204&q=${city}`;
+    let resData=await getData(url);
+    let logData=await resData.json();
+    displayData(logData);
+}
+
+form.addEventListener("submit",(e)=>formSub(e));
 
 const getData= async (url)=>{
     let data= await fetch(url,{
         mode: "cors"
     });
     return data;
-    console.log(data);
 }
 
 const displayData=(data)=>{
