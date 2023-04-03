9eb11bb7a865920113e6284dc5f9d9d06a7a21d0
nicolas ngwira <ngwiranicolas@gmail.com>
2023-04-03 22:03:34 +0200

    modified all files

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let dataHolder=document.getElementById("content");
 let form=document.getElementById("search")
+let loader=document.getElementById("load");
 
 const formSub=async (e)=>{
     e.preventDefault();
-    dataHolder.innerHTML=``;
+    loader.style.display="block";
+    
     let formData=new FormData(form);
     let city=formData.get("city");
 
@@ -11,6 +13,7 @@ const formSub=async (e)=>{
     let resData=await getData(url);
     let logData=await resData.json();
     displayData(logData);
+    console.log(logData);
 }
 
 form.addEventListener("submit",(e)=>formSub(e));
@@ -22,11 +25,14 @@ const getData= async (url)=>{
     return data;
 }
 
-const displayData=(data)=>{
-    const keyNames=Object.keys(data);
+const displayData=({current})=>{
 
+    loader.style.display="none";
+    dataHolder.innerHTML=``;
+
+    const keyNames=Object.keys(current);
     keyNames.map((item,i)=>{
-
+                 
         let dataItem=document.createElement("div")
         dataItem.classList.add("dataItem");
         
@@ -36,10 +42,19 @@ const displayData=(data)=>{
 
         let itemContent=document.createElement("div");
         itemContent.classList.add("itemContent");
-        itemContent.innerText=`${data[item]}`;
+        itemContent.innerText=`${current[item]}`;
         
         dataItem.appendChild(title);
         dataItem.appendChild(itemContent);
         dataHolder.appendChild(dataItem);
     })
 }
+
+(async ()=>{
+    loader.style.display="block";
+    let url=`http://api.weatherapi.com/v1/current.json?key=91de4c405b304512a41210049230204&q=lilongwe`;
+    let resData=await getData(url);
+    let logData=await resData.json();
+    displayData(logData);
+    console.log(logData);
+})();
